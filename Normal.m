11b855820b614a455a2a81649c5853a23d16b008
yousef saeed <yousef_said_99@icloud.com>
2024-11-15 01:50:15 +0200

    Normal

--- Normal.m
+++ Normal.m
@@ -1,9 +1,9 @@
 % Parameters for the normal distribution
 mu = -8;     % Mean
-sigma = 2;   % Standard deviation (square root of variance, which is 4)
+sigma = 4 ;   % Standard deviation (square root of variance, which is 4)
 
 % Generate normally distributed data
-Y = normrnd(mu, sigma, [1, 100000]);
+Y = normrnd(mu, sigma, [1, 1000000]);
 
 % Display or save the data as needed
 disp(Y);
